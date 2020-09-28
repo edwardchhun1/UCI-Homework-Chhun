bdca5f70212fbd0df07c6b32a9588572ad3af56f
unknown <edwardchhun@gmail.com>
2020-09-27 20:48:04 -0700

    not setting to 0 - creating lists now

--- Python-Challenge/PyBank/main.py
+++ Python-Challenge/PyBank/main.py
@@ -3,22 +3,21 @@ import csv
 
 csvpath = os.path.join("..", "..", "..", "..", "..", "Desktop", "PyBank.csv")
 
+#Creating list variables for storing results - not setting to 0 anymore
+    total_months = []
+    total_net = []
+    average_change = []
+
 #open csv 
 with open(csvpath) as bankbudget:
     csvreader = csv.reader(bankbudget, delimiter=',')
 
     #Skip header as part of count
     csv_header = next(csvreader)
-
-
-    #setting each variable to 0 
-    total_months = 0
-    total_net = 0
     
-
-
-    
+    # Reading row by row for results
     for row in csv.reader(bankbudget):
+       
         #Total Months Calculation
         total_months.append(row[0])
 
