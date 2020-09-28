51994f8edbcced0443f66c27a4d9d35597182af5
unknown <edwardchhun@gmail.com>
2020-09-27 22:25:26 -0700

    passed qa - printing fully formatted output

--- Python-Challenge/PyPoll/main.py
+++ Python-Challenge/PyPoll/main.py
@@ -4,9 +4,9 @@ import csv
 csvpath = os.path.join("..", "..", "..", "..", "..", "Desktop", "PyPoll.csv")
 
 # Creating list variables for storing results
-total_votes = []
+total_votes = 0
 unique_candidates = []
-vote_count_candidates = []
+vote_count = []
 
 #open csv 
 with open(csvpath) as pollcount:
@@ -18,12 +18,41 @@ with open(csvpath) as pollcount:
     # Reading row by row for results
     for row in csv.reader(pollcount):
     
-    #Total Months Calculation
-        total_votes.append(row[0])
+    #Total Votes Calculation
+        total_votes += 1
 
-    print("Election Results")
-    print("----------------------")
-    print(f"Total Votes: {len(total_votes)}")
-    print("----------------------")
-    print("----------------------")
-    print("----------------------")
+    #Unique Candidates and totals for each candidate - Row 2 = utilize column 3
+        candidates = (row[2])
+       
+        # indexing/storing unique candidates in list - each time unique value shows up add 1 to vote count
+        if candidates in unique_candidates:
+            candidate_index = unique_candidates.index(candidates)
+            vote_count[candidate_index] = vote_count[candidate_index] + 1
+        else:
+            unique_candidates.append(candidates)
+            vote_count.append(1)
+
+percent = []
+max_votes = vote_count[0]
+max_index = 0
+
+for i in range(len(unique_candidates)):
+    #calculating percentage - i to continue looping
+    vote_percentage = round(vote_count[i]/total_votes*100, 2)
+    percent.append(vote_percentage)
+
+
+#declaring winner variable
+election_winner = unique_candidates[max_index]
+
+#Print Output
+print("Election Results")
+print("----------------------")
+print(f"Total Votes: {total_votes}")
+print("----------------------")
+#printing loop results for candidates
+for i in range(len(unique_candidates)):
+    print(f"{unique_candidates[i]} : {percent[i]}% ({vote_count[i]})")
+print("----------------------")
+print(f"Winner: {election_winner}")
+print("----------------------")
