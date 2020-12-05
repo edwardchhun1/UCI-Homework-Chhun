e5b29c93fa6ee92cc80c077e6aabe6475a3cc3d9
unknown <edwardchhun@gmail.com>
2020-12-05 10:38:42 -0800

    create engine created - headers corrected - data loaded via Create Engine

--- ETL-Group-Project/GDP_Life_Exp.sql
+++ ETL-Group-Project/GDP_Life_Exp.sql
@@ -22,8 +22,16 @@ create table GDP_Life (
 	id SERIAL PRIMARY KEY,
 	"Country Name" VARCHAR not NULL,
 	"Country Code" VARCHAR(5) not NULL,
-	"2008 GDP (Dollars)" VARCHAR(100), 
-    "2009 GDP (Dollars)" VARCHAR(100),
-    "2010 GDP (Dollars)" VARCHAR(100),
-	"2011 GDP (Dollars)" VARCHAR(100)
+	"2008 GDP in Dollars" VARCHAR(100), 
+    "2009 GDP in Dollars" VARCHAR(100),
+    "2010 GDP in Dollars" VARCHAR(100),
+	"2011 GDP in Dollars" VARCHAR(100),
+	"2008 Life Expectancy in Years" VARCHAR(100),
+	"2009 Life Expectancy in Years" VARCHAR(100),
+	"2010 Life Expectancy in Years" VARCHAR(100),
+	"2011 Life Expectancy in Years" VARCHAR(100)
 );
+
+select * from GDP;
+select * from life_expectancy;
+select * from gdp_life;
